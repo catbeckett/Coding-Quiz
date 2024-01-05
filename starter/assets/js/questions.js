6dde55deba350a957ce876ce48d78d98ec316a83
Cat <catejb89@gmail.com>
2024-01-05 18:03:55 +0000

    starter/assets/js/questions.js

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,18 +1,29 @@
-// variable for questions array
-
+  // variable for questions array
+  
 let questionList = [
-    {
-        title: 'What does HTML stand for?',
-        choices: ['Hyper Text Markup Language', 'High-level Text Machine Language', 'Hyperlink and Text Markup Language', 'Home Tool Markup Language'],
-        correctAnswer: 0,
-    },
-    {
-        title: 'Again, what does HTML stand for?',
-        choices: ['High-level Text Machine Language', 'Hyperlink and Text Markup Language', 'Hyper Text Markup Language', 'Home Tool Markup Language'],
-        correctAnswer: 2,
-    },
-]
-
-// question
-// 4 answers
-// key: answer && value: true/false
+  {
+      title: 'What does HTML stand for?',
+      choices: ['Hyper Text Markup Language', 'High-level Text Machine Language', 'Hyperlink and Text Markup Language', 'Home Tool Markup Language'],
+      correctAnswer: 0,
+  },
+  {
+      title: 'How can we style a website?',
+      choices: ['wrting in a README.md file', 'by adding Javascript in a script', 'by adding CSS in a style sheet', 'None of these'],
+      correctAnswer: 2,
+  },
+  {
+      title: 'What words can we use to declare variables?',
+      choices: ['constant', 'let and variable', 'let and const', 'var'],
+      correctAnswer: 2,
+  },
+  {
+      title: 'Stored values are coalled strings. What do we store strings in?',
+      choices: ['brakets', 'double quotes', 'curly braces', 'single quotes'],
+      correctAnswer: 1,
+  },
+  {
+      title: 'What does const stand for?',
+      choices: ['constant', 'continous', 'Constantinople', 'constable'],
+      correctAnswer: 0,
+  },
+];
